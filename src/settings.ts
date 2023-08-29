3142094bc048eeaf374e23737afd8ea5c0477421
Daniel Ma <dma@planningcenter.com>
2023-08-28 18:44:44 -0700

    Rename
    So I can play with it

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'HomebridgePluginDwelo';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@eduardoramirez/homebridge-dwelo';
+export const PLUGIN_NAME = '@danielma/homebridge-dwelo';
